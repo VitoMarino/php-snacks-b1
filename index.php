dbd11ff1b85de082286f1ff6ee8523c1acd88733
VitoMarino <marino.vito2001@gmail.com>
2024-06-26 12:29:43 +0200

    Snack

--- index.php
+++ index.php
@@ -1,4 +1,26 @@
-//*Snack 1
-//*Creiamo un array contenente le partite di basket di un'ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema:
-//*Olimpia Milano - Cantù | 55-60 
+// *Snack 1
+// *Creiamo un array contenente le partite di basket di un'ipotetica tappa del calendario. 
+//* Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema:
+// *Olimpia Milano - Cantù | 55-60
 
+<?php
+
+$partite = [
+    "squadraCasa" => "Inter",
+    "squadraOspite" => "Milan",
+],
+
+[
+    "squadraCasa" => "Lazio",
+    "squadraOspite" => "Juventus",
+],
+
+[
+    "squadraCasa" => "Napoli",
+    "squadraOspite" => "Roma",
+];
+
+echo $partite["puntiCasa"] = rand(1, 60);
+echo $partite["puntiOspite"] = rand(1, 60);
+
+?>

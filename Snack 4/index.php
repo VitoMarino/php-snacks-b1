e28ac8349f02356f0bd7db6dc449e26990cd165f
VitoMarino <marino.vito2001@gmail.com>
2024-06-26 15:02:35 +0200

    preparazione snack

---
+++ Snack 4/index.php
@@ -0,0 +1,22 @@
+<?php 
+/* Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta*/
+
+$titleHeader= "Snack 4";
+
+?>
+
+<!DOCTYPE html>
+<html lang="ita">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 4</title>
+</head>
+<body>
+    <header>
+        <h1>
+            <?php echo $titleHeader ?>
+        </h1>
+    </header>
+</body>
+</html>

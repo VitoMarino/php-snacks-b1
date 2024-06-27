d043153eff3d32d106543d36248cae2311efeedd
VitoMarino <marino.vito2001@gmail.com>
2024-06-27 12:31:48 +0200

    Snack 7

--- Snack 7/index.php
+++ Snack 7/index.php
@@ -4,7 +4,7 @@ Creare un array contenente qualche alunno di un'ipotetica classe. Ogni alunno av
 Questo array dovra' essere inserito in un file separato, da importare.
 Stampare Nome, Cognome e la media dei voti di ogni alunno.*/
 
-inlcude_once __DIR__ . "/voti.php";
+include_once __DIR__ . "/voti.php";
 
 $alunni = [
     "nome" => "Gino",

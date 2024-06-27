404c0d3bf3248110ac1ad31de72660017e2f26c1
VitoMarino <marino.vito2001@gmail.com>
2024-06-27 12:30:29 +0200

    Snack 7

---
+++ Snack 7/index.php
@@ -0,0 +1,24 @@
+<?php
+/*Snack 7
+Creare un array contenente qualche alunno di un'ipotetica classe. Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. 
+Questo array dovra' essere inserito in un file separato, da importare.
+Stampare Nome, Cognome e la media dei voti di ogni alunno.*/
+
+inlcude_once __DIR__ . "/voti.php";
+
+$alunni = [
+    "nome" => "Gino",
+    "cognome" => "Ginotti",
+
+    "nome" => "Gina",
+    "cognome" => "Ginassi",
+
+    "nome" => "Giovy",
+    "cognome" => "Giovanotti",
+];
+
+var_dump($alunni);
+
+var_dump($votiAlunni);
+
+?>
---
+++ Snack 7/voti.php
@@ -0,0 +1,9 @@
+<?php
+
+$votiAlunni= [
+    "votoUno" => 3,
+    "votoDue" => 6,
+    "votoTre" => 10
+];
+
+?>

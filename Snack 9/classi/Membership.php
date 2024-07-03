6c0e85213227ae8f2d0a24dfe6a8b160f8d9f256
VitoMarino <marino.vito2001@gmail.com>
2024-07-03 12:28:40 +0200

    MemberShip.php

---
+++ Snack 9/classi/Membership.php
@@ -0,0 +1,12 @@
+<?php 
+
+require_once __DIR__ . "./User.php";
+require_once __DIR__ ."./PremiumUser.php";
+
+class Memberchip extends PremiumUser{
+    private $prezzoSpecial;
+
+    
+}
+
+?>
--- Snack 9/classi/PremiumUser.php
+++ Snack 9/classi/PremiumUser.php
@@ -1,11 +1,16 @@
 <?php 
-
+require_once __DIR__ . "./User.php";
 class PremiumUser extends User {
     private $offerta;
 
     public function __construct($offerta){
-    $this->offerta = $offerta;
+        
+        $this->offerta = $offerta;
     }
+
+    public function getOfferta(){
+        return $this->offerta;
+}
 }
 
 ?>
